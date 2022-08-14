cd371608c33997b85273206e5d74f34434835408
rutuja_mali <rutujamali111@gmail.com>
2022-08-14 06:58:29 +0530

    Choose letter

--- src/main/java/bridgelabzs/TicTacToeGame.java
+++ src/main/java/bridgelabzs/TicTacToeGame.java
@@ -1,10 +1,36 @@
 package bridgelabzs;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
+	static char[] board = new char[10];
+	static char userLetter;
+    static char computerLetter;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 
+		 createEmptyBoard();
+		 chooseLetter();
 
+
+	}
+	private static void chooseLetter() {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a letter :: X or O : ");
+        userLetter = scanner.next().toUpperCase().charAt(0);
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
+        scanner.close();
+		
+	}
+	private static void createEmptyBoard() {
+		// TODO Auto-generated method stub
+		for(int index=1; index<board.length;index++)
+        {
+            board[index] = ' ';
+        }
+		
 	}
 
 }
