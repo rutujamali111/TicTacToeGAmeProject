16423146ec2315c4cd202f42a8227195ee48b337
rutuja_mali <rutujamali111@gmail.com>
2022-08-14 07:04:27 +0530

    show board

--- src/main/java/bridgelabzs/TicTacToeGame.java
+++ src/main/java/bridgelabzs/TicTacToeGame.java
@@ -12,8 +12,18 @@ public class TicTacToeGame {
 		 
 		 createEmptyBoard();
 		 chooseLetter();
+		 showBoard();
 
 
+	}
+	private static void showBoard() {
+		// TODO Auto-generated method stub
+		System.out.println( board[1] + " | " + board[2] + " | " + board[3] );
+        System.out.println("----------");
+        System.out.println( board[4] + " | " + board[5] + " | " + board[6] );
+        System.out.println("----------");
+        System.out.println( board[7] + " | " + board[8] + " | " + board[9] );
+		
 	}
 	private static void chooseLetter() {
 		// TODO Auto-generated method stub
